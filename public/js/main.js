11159851654d0f8c73818a5e43b2626d59f5248d
dmdeh <131264106+dmdeh@users.noreply.github.com>
2024-04-09 14:32:23 +0900

    feat: news title list 구성

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,12 @@
-console.log("hello");
+fetch("/users")
+  .then((response) => response.json())
+  .then((data) => {
+    const titleList = document.querySelector('.title-list');
+    data.articles.forEach((item) => {
+      titleList.innerHTML += `
+      <p>${item.title}</p>
+      `;
+      console.log(item.title);
+    });
+  })
+  .catch((error) => console.error("Error fetching news:", error));
