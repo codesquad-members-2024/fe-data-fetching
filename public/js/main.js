c8287dfc071d502c72578895ce9ebf30d6871846
dmdeh <131264106+dmdeh@users.noreply.github.com>
2024-04-09 16:42:14 +0900

    feat: 뉴스 제목 선택 시 콘텐츠 보여주기

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,3 @@
-fetch("/users")
-  .then((response) => response.json())
-  .then((data) => {
-    const titleList = document.querySelector('.title-list');
-    data.articles.forEach((item) => {
-      titleList.innerHTML += `
-      <p>${item.title}</p>
-      `;
-      console.log(item.title);
-    });
-  })
-  .catch((error) => console.error("Error fetching news:", error));
+import getNewsData from "./news.js";
+
+getNewsData();
---
+++ public/js/news.js
@@ -0,0 +1,26 @@
+function getNewsData() {
+  fetch("/users")
+    .then((response) => response.json())
+    .then((data) => {
+      const titleList = document.querySelector(".title-list");
+      const newsArticle = document.querySelector(".news-article");
+      data.articles.forEach((item) => {
+        titleList.innerHTML += `<p>${item.title}</p>`;
+      });
+
+      titleEventListener(titleList, newsArticle, data);
+    })
+    .catch((error) => console.error("Error fetching news:", error));
+}
+
+function titleEventListener(titleList, newsArticle, data) {
+  titleList.addEventListener("click", function (event) {
+    if (event.target.tagName === "P") {
+      const index = Array.from(this.children).indexOf(event.target);
+      const clicked = data.articles[index].description;
+      newsArticle.innerHTML = `<p>${clicked}</p>`;
+    }
+  });
+}
+
+export default getNewsData;
