30356063eeb89b81d1286506e5fb001daad5e543
정채영 <chaeyoung@jeongchaeyeong-ui-MacBookAir.local>
2024-04-10 19:23:25 +0900

    feat: 선택한 뉴스 콘텐츠 서버에 요청해서 받기

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const newsRoutes = require("./routes/index");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/news", newsRoutes);
+
+const fs = require("fs");
+
+app.get("/news/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const filePath = path.join(__dirname, "data/headlines.json");
+    const data = fs.readFileSync(filePath, "utf-8");
+    const newsData = JSON.parse(data);
+    const newsArticles = newsData.articles;
+    res.render("selectedNews", { newsArticles, id });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+});
+
+app.listen(8080, () => {
+  console.log("listening on port 8080!!");
+});
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+const fs = require("fs");
+const path = require("path");
+
+router.get("/news", async (req, res) => {
   try {
-    const response = await fetch(
-      "https://newsapi.org/v2/top-headlines?country=kr&apiKey=c872dc5c3a7546958dcb2c8b41c4d624"
-    );
-    if (response.ok) {
-      const newsData = await response.json();
-      const newsArticles = newsData.articles;
-      res.render("home", { newsArticles });
-    } else {
-      console.error("Failed to update news");
-      res.status(500).send("Failed to update news");
-    }
+    const filePath = path.join(__dirname, "../data/headlines.json");
+    const data = fs.readFileSync(filePath, "utf-8");
+    const newsData = JSON.parse(data);
+    const newsArticles = newsData.articles;
+    res.render("home", { newsArticles });
   } catch (error) {
     console.error("Error fetching data:", error);
     res.status(500).json({ error: "Failed to fetch data" });
