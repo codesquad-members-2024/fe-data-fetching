50e628b9e94f99cc5b0fca98c9d3d89db3085ae6
정채영 <chaeyoung@jeongchaeyeong-ui-MacBookAir.local>
2024-04-11 01:58:15 +0900

    fix: 외부 api 사용하지 않고 초기 페이지만 렌더링

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,7 @@ const path = require("path");
 
 router.get("/", async (req, res) => {
   try {
-    const filePath = path.join(__dirname, "../data/headlines.json");
-    const data = fs.readFileSync(filePath, "utf-8");
-    const newsData = JSON.parse(data);
-    const newsArticles = newsData.articles;
-    res.render("home", { newsArticles });
+    res.render("home");
   } catch (error) {
     console.error("Error fetching data:", error);
     res.status(500).json({ error: "Failed to fetch data" });
