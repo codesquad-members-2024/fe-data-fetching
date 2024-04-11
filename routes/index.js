9095e973f645de878277e943847c717f17a2a6da
정채영 <chaeyoung@jeongchaeyeong-ui-MacBookAir.local>
2024-04-11 11:54:28 +0900

    refactor: try-catch 블록 제거
    데이터를 가져오는 과정이 없기 때문

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const fs = require("fs");
-const path = require("path");
-
 router.get("/", async (req, res) => {
-  try {
-    res.render("home");
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    res.status(500).json({ error: "Failed to fetch data" });
-  }
+  res.render("home");
 });
 
 module.exports = router;
