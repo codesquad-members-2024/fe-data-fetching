fc42217896a0f17bf8f04847f7ada6a2afccdbf0
minjeongHEO <hmjoung9770@gmail.com>
2024-04-12 11:56:41 +0900

    feat: 선택된 뉴스 텍스트 볼드 적용

--- public/js/contents.js
+++ public/js/contents.js
@@ -1,6 +1,14 @@
+const listTextBold = (id) => {
+    const listTarget = document.querySelector('.section__left__news_title');
+    Array.from(listTarget.children).forEach((li) => {
+        if (li.getAttribute('article-id') !== id.toString()) li.classList.remove('selected');
+        if (li.getAttribute('article-id') === id.toString()) li.classList.add('selected');
+    });
+};
+
 const createLi = (content) => {
     const { id, title } = content;
-    return `<li id="${id}">
+    return `<li article-id="${id}" class="">
                 <a href="#" class="news_title__link__item" role="tab" aria-selected="false">
                     <span>${title}</span>
                 </a>
@@ -18,9 +26,11 @@ const createListHTML = (newsDatas) => {
     listTarget.innerHTML = listHTML;
 };
 
-const createDetailNewsHTML = ({ content }) => {
+const createDetailNewsHTML = ({ content, id }) => {
     const detailNewsTarget = document.querySelector('.section__right__article');
     detailNewsTarget.innerHTML = content;
+    detailNewsTarget.setAttribute('article-id', id);
+    listTextBold(id);
 };
 
 export { createListHTML, createDetailNewsHTML };
